d6f2c627dd53bff8f78e8e4ccf59de0900f39442
Kwinten Pisman <kwinten.pisman@gmail.com>
2021-08-12 10:53:13 +0200

    qsdf

--- clone.case.js
+++ clone.case.js
@@ -1,5 +1,5 @@
 const child_process = require('child_process');
 
 const cp = child_process.spawn('git', ['clone', 'https://github.com/kwintenp/import-repo-test'], {
-    stdio: 'inherit'
+    stdio: 'inherit'sss
 });
